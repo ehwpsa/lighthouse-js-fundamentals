a7eddde81b7829b590567d26f12c8c54c9004bc6
Justin Hanna <justinhannadesu@gmail.com>
2017-10-27 19:00:17 +0000

    Adds conditional exercises

--- odd.js
+++ odd.js
@@ -4,3 +4,25 @@ function isOdd(num) {
 
 console.log("3 is odd: " + isOdd(3));
 console.log("8 is odd: " + isOdd(8));
+
+
+function ageCalculator(name,yearOfBirth,currentYear){
+	var h = currentYear - yearOfBirth;
+    return `${name} is ${h} years old.`
+    
+}
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
+
+console.log('--------------------------------')
+
+function howManyHundreds(num){
+	var h = num / 100;
+	return Math.floor(h);
+	}
+
+console.log(howManyHundreds(1000));
+console.log(howManyHundreds(894));
+console.log(howManyHundreds(520));
+console.log(howManyHundreds(99));
+console.log(howManyHundreds(0));
